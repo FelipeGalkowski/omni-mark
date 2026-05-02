4a451d22c177659194e2344dd7e1843f33e4d87c
Felipe Galkowski <165969310+FelipeGalkowski@users.noreply.github.com>
2026-05-02 18:01:27 -0300

    feat: update structure git pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/dash-test',
+  base: '/omni-mark',
 
   resolve: {
     alias: {
