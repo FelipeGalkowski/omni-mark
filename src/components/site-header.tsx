5f0a533e72ae2fa94573c3d31b8bc5ab658fab34
Nicolas Junkes <nicolas.junkes00@gmail.com>
2026-05-03 22:44:49 -0300

    feat: add a button to authenticate the Meli user account

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,8 +1,18 @@
 import { Separator } from "@/components/ui/separator"
 import { Button } from "./ui/button"
+import { useAuth } from "@/hooks/useAuth";
 
 export function SiteHeader() {
-  const authenticationUrl = "https://auth.mercadolivre.com.br/authorization?response_type=code&client_id=4933724664056622&redirect_uri=https://felipegalkowski.github.io/omni-mark/"
+  const { signed, setSignedIn, signIn } = useAuth();
+
+  async function handleSignIn() {
+    const redirectUrl = await signIn();
+    console.log(redirectUrl);
+
+    if (redirectUrl) {
+      globalThis.location.href = redirectUrl;
+    };
+  }
 
   return (
     <header className="flex h-12 shrink-0 items-center gap-2 border-b ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
@@ -13,11 +23,23 @@ export function SiteHeader() {
         />
         <h1 className="text-base font-medium">Dashboard</h1>
       </div>
-      <Button
-        onClick={() => globalThis.location.href = authenticationUrl}
-      >
-        Login
-      </Button>
+      {signed && (
+        <>
+          <span>Logado</span>
+          <Button
+            onClick={() => setSignedIn(false)}
+          >
+            Sair
+          </Button>
+        </>
+      )}
+      {!signed && (
+        <Button
+          onClick={handleSignIn}
+        >
+          Login
+        </Button>
+      )}
     </header>
   )
 }
---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,21 @@
+import { SectionCards } from "@/components/section-cards"
+import { SiteHeader } from "@/components/site-header"
+
+export function Dashboard() {
+  // const { orders } = useGetOrders(sellerId);
+
+  // console.log({ orders })
+
+  return (
+    <>
+      <SiteHeader />
+      <div className="flex flex-1 flex-col">
+        <div className="@container/main flex flex-1 flex-col gap-2">
+          <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+            <SectionCards />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
