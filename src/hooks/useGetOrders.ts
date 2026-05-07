19497a844da82c13eec1e52680f47fd61cdc83f3
Felipe Galkowski <165969310+FelipeGalkowski@users.noreply.github.com>
2026-05-07 20:09:04 -0300

    feat: fix orders query reactivity using auth context

--- src/hooks/useGetOrders.ts
+++ src/hooks/useGetOrders.ts
@@ -1,15 +1,15 @@
 import { OrdersService } from "@/service/OrdersService";
 import { useQuery } from "@tanstack/react-query";
-import { storageKeys } from "@/config/storageKeys";
+import { useAuth } from "@/hooks/useAuth";
 
 export function useGetOrders() {
-  const token = sessionStorage.getItem(storageKeys.accessToken);
+  const { signed } = useAuth();
 
   const { data } = useQuery({
     queryKey: ["orders"],
     queryFn: async () => await OrdersService.getOrders(),
     staleTime: 1 * 60 * 1000,
-    enabled: !!token
+    enabled: signed
   });
 
   return {
@@ -19,13 +19,13 @@ export function useGetOrders() {
 }
 
 export function useGetOrdersCount() {
-  const token = sessionStorage.getItem(storageKeys.accessToken);
+  const { signed } = useAuth();
 
   const { data } = useQuery({
     queryKey: ["orders-count"],
     queryFn: async () => await OrdersService.getOrdersCount(),
     staleTime: 1 * 60 * 1000,
-    enabled: !!token
+    enabled: signed
   });
 
   return {
